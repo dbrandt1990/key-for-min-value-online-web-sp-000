deab7983e376568220d7e2e4211947be1d717e8a
Danny Brandt <dvbrandt90@gmail.com>
2020-12-15 23:15:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(name_hash)
 if !name_hash
   nil
 else
-  min_value = 1000000000
+  min_value = ''
   min_key = nil
   name_hash.each do |key, value|
-    if value < min_value
+    if value < min_value || min_value == ''
       min_value = value
       min_key = key
     end
